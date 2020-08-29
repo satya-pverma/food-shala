582eb2769252bef17e6973f3881c4d99da3a3c05
satya-pverma <satyaverma0075@gmail.com>
2020-08-29 13:23:51 +0530

    error resolving

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 5050,
-    MONGODB_URL: "mongodb+srv://shopeasy:o9ahhIhx23IZWAxQ@cluster0.d0nxu.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://shopeasy:o9ahhIhx23IZWAxQ@cluster0.d0nxu.mongodb.net/<dbname>?retryWrites=true&w=majority",
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
     PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
 
